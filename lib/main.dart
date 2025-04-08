5bc5bc92e0502357339e6a96ff73c48859f2a24a
TahaSamji <tahashamji45@gmail.com>
2025-04-08 16:12:41 +0500

    Project Initialized

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HospitalPortal());
+}
+
+class HospitalPortal extends StatelessWidget {
+  const HospitalPortal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
